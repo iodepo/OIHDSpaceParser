1dc2d4e96325c3bfc0e0b67312b4ff49125bdf18
a.lambert@unesco.org <r3d!zeb?raGi>
2021-02-25 17:00:45 +0100

    some more example url's

--- scripts/config.inc.php
+++ scripts/config.inc.php
@@ -9,6 +9,10 @@
 
 //for the staging server
 $repoURL = 'http://iodeaquadocs.staging.openrepository.com/oai/request?verb=ListRecords&metadataPrefix=oai_dc';
+//next pages are something like
+//$repoURL = 'http://iodeaquadocs.staging.openrepository.com/oai/request?verb=ListRecords&resumptionToken=oai_dc////100';
+//$repoURL = 'http://iodeaquadocs.staging.openrepository.com/oai/request?verb=ListRecords&resumptionToken=oai_dc////200';
+//$repoURL = 'http://iodeaquadocs.staging.openrepository.com/oai/request?verb=ListRecords&resumptionToken=oai_dc////900';
 
 //the real one
 //$repoURL = 'http://aquadocs.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc';
