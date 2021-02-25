f39002699b4582c74f7daf3762eb4359f4d913d4
a.lambert@unesco.org <r3d!zeb?raGi>
2021-02-25 12:09:41 +0100

    config file (not sure yet if this is overkill)

---
+++ scripts/config.inc.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * hold the config settings for the parser
+ * no idea if that is really needed at this stage
+ *
+ * @author Arno Lambert <a.lambert@unesco.org>
+ * @since 24/02/2021
+ */
+
+//for the staging server
+$repoURL = 'http://iodeaquadocs.staging.openrepository.com/oai/request?verb=ListRecords&metadataPrefix=oai_dc';
+
+//the real one
+//$repoURL = 'http://aquadocs.org/oai/request?verb=ListRecords&metadataPrefix=oai_dc';
