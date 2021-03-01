24600af5b081c5827f993497616dfdff8d471559
a.lambert@unesco.org <r3d!zeb?raGi>
2021-03-01 14:35:45 +0100

    try to catch problems before doing stupid things

--- scripts/parser.php
+++ scripts/parser.php
@@ -26,181 +26,187 @@ while ($nextRepoUrl != '') {
      * get the complete page in one long string
      * replace the dublin core tags with something without prefixes
      */
-    $page = file_get_contents($nextRepoUrl);
-    $patterns = array(
-        '/<oai_dc:dc\s/',
-        '/<dc:/',
-        '/<\/oai_dc:dc/',
-        '/<\/dc:/',
-    );
-    $replace = array(
-        '<oai_dc_dc ',
-        '<dc_',
-        '</oai_dc_dc',
-        '</dc_',
-    );
-    $page = preg_replace($patterns, $replace, $page);
+    try {
+        $page = file_get_contents($nextRepoUrl);
 
-    //file_put_contents('output.xml', $page);
-    $oai = new SimpleXMLElement($page);
-
-    print $oai->responseDate;
-
-    foreach ($oai->ListRecords->record as $record) {
-        //print "\n*************************\n";
-        //print $record->header->identifier . ' ' . $record->header->datestamp . "\n";
-
-        //substract the handle id from the identifier
-        $identifier = $record->header->identifier;
-        $idSuffix = preg_replace(
-            '/.+?:(\d+\/\d+)/',
-            "$1",
-            $identifier
-        );
-
-        //to what (sub)sets belongs this document
-        foreach ($record->header->setSpec as $set) {
-            //print $set . "\n";
-        }
-
-        //read all the metadata and put the values in vars ready for output
-
-        //we often use the same regex
-        $pattern = array(
-            '"',
-            "\n",
-            "\r"
+        $patterns = array(
+            '/<oai_dc:dc\s/',
+            '/<dc:/',
+            '/<\/oai_dc:dc/',
+            '/<\/dc:/',
         );
         $replace = array(
-            '\"',
-            '',
-            ''
+            '<oai_dc_dc ',
+            '<dc_',
+            '</oai_dc_dc',
+            '</dc_',
         );
-        $metaData = $record->metadata->oai_dc_dc;
-        $name = trim($metaData->dc_title);
-        $name = str_replace(
-            $pattern,
-            $replace,
-            $name
-        );
+        $page = preg_replace($patterns, $replace, $page);
 
-        //get all the descriptions
-        //put them in one long description
-        $descriptions = array();
-        $descriptionString = '';
-        foreach ($metaData->dc_description as $description) {
-            $description = str_replace(
-                $pattern,
-                $replace,
-                trim($description)
+        //file_put_contents('output.xml', $page);
+        $oai = new SimpleXMLElement($page);
+
+        print $oai->responseDate;
+
+        foreach ($oai->ListRecords->record as $record) {
+            //print "\n*************************\n";
+            //print $record->header->identifier . ' ' . $record->header->datestamp . "\n";
+
+            //substract the handle id from the identifier
+            $identifier = $record->header->identifier;
+            $idSuffix = preg_replace(
+                '/.+?:(\d+\/\d+)/',
+                "$1",
+                $identifier
             );
-            $descriptions[] = ' - ' . $description . ' - ';
-        }
-        $descriptionString = implode(', ', $descriptions);
 
-        //this is the link to the document
-        $url = 'https://www.oceandocs.org/handle/' . $idSuffix;
-
-        //get all the contributors
-        $contributors = array();
-        foreach ($metaData->dc_creator as $contributor) {
-            $contributor = str_replace(
-                $pattern,
-                $replace,
-                trim($contributor)
-            );
-            $contributors[] = $contributor;
-        }
-
-        //get all the keywords
-        $keywords = array();
-        $keywordString = '';
-        foreach ($metaData->dc_subject as $keyword) {
-            $keyword = str_replace(
-                $pattern,
-                $replace,
-                trim($keyword)
-            );
-            $keywords[] = '"' . $keyword . '"';
-        }
-        $keyword = implode(', ', $keywords);
-
-        //special case for contributor
-        $publisher = '';
-        if (isset($metaData->dc_publisher)) {
-            $publisher = $metaData->dc_publisher;
-            $publisher = str_replace(
-                $pattern,
-                $replace,
-                trim($publisher)
-            );
-        }
-
-        $JSON = <<<EOT
-{
-    "@context": {
-        "@vocab": "https://schema.org/"
-    },
-    "@type": "CreativeWork",
-    "@id": "$identifier",
-    "name": "$name",
-    "description": "$descriptionString",
-    "url":  "$url",
-    "identifier":
-      {
-        "@id": "https://handle.net/$idSuffix",
-        "@type": "PropertyValue",
-        "propertyID": "https://handle.net/",
-        "value": "$idSuffix",
-        "url": "https://handle.net/$idSuffix"
-      }
-EOT;
-        if (isset($contributors)
-         && count($contributors)
-        ) {
-            foreach ($contributors as $contributor) {
-                $JSON .= <<< EOT2
-    ,"contributor": {
-      "@type": "Person",
-      "name": "$contributor"
-    }
-EOT2;
+            //to what (sub)sets belongs this document
+            foreach ($record->header->setSpec as $set) {
+                //print $set . "\n";
             }
+
+            //read all the metadata and put the values in vars ready for output
+
+            //we often use the same regex
+            $pattern = array(
+                '"',
+                "\n",
+                "\r"
+            );
+            $replace = array(
+                '\"',
+                '',
+                ''
+            );
+            $metaData = $record->metadata->oai_dc_dc;
+            $name = trim($metaData->dc_title);
+            $name = str_replace(
+                $pattern,
+                $replace,
+                $name
+            );
+
+            //get all the descriptions
+            //put them in one long description
+            $descriptions = array();
+            $descriptionString = '';
+            foreach ($metaData->dc_description as $description) {
+                $description = str_replace(
+                    $pattern,
+                    $replace,
+                    trim($description)
+                );
+                $descriptions[] = ' - ' . $description . ' - ';
+            }
+            $descriptionString = implode(', ', $descriptions);
+
+            //this is the link to the document
+            $url = 'https://www.oceandocs.org/handle/' . $idSuffix;
+
+            //get all the contributors
+            $contributors = array();
+            foreach ($metaData->dc_creator as $contributor) {
+                $contributor = str_replace(
+                    $pattern,
+                    $replace,
+                    trim($contributor)
+                );
+                $contributors[] = $contributor;
+            }
+
+            //get all the keywords
+            $keywords = array();
+            $keywordString = '';
+            foreach ($metaData->dc_subject as $keyword) {
+                $keyword = str_replace(
+                    $pattern,
+                    $replace,
+                    trim($keyword)
+                );
+                $keywords[] = '"' . $keyword . '"';
+            }
+            $keyword = implode(', ', $keywords);
+
+            //special case for contributor
+            $publisher = '';
+            if (isset($metaData->dc_publisher)) {
+                $publisher = $metaData->dc_publisher;
+                $publisher = str_replace(
+                    $pattern,
+                    $replace,
+                    trim($publisher)
+                );
+            }
+
+            $JSON = <<<EOT
+    {
+        "@context": {
+            "@vocab": "https://schema.org/"
+        },
+        "@type": "CreativeWork",
+        "@id": "$identifier",
+        "name": "$name",
+        "description": "$descriptionString",
+        "url":  "$url",
+        "identifier":
+          {
+            "@id": "https://handle.net/$idSuffix",
+            "@type": "PropertyValue",
+            "propertyID": "https://handle.net/",
+            "value": "$idSuffix",
+            "url": "https://handle.net/$idSuffix"
+          }
+    EOT;
+            if (isset($contributors)
+                && count($contributors)
+            ) {
+                foreach ($contributors as $contributor) {
+                    $JSON .= <<< EOT2
+        ,"contributor": {
+          "@type": "Person",
+          "name": "$contributor"
+        }
+    EOT2;
+                }
+            }
+
+            if (isset($publisher)
+                && $publisher != ''
+            ) {
+                $JSON .= <<< EOT3
+        ,"contributor": {
+          "@type": "Organization",
+          "legalName": "$publisher"
+        }
+    EOT3;
+            }
+
+            if (isset($keywordString)
+                && $keywordString != ''
+            ) {
+                $JSON .= <<< EOT4
+        ,"keywords": [$keywordString]
+    EOT4;
+            }
+            $JSON .= '
+    }';
+            $outputJSON[] = $JSON;
         }
 
-        if (isset($publisher)
-            && $publisher != ''
+        //make the url for the next page if there is any
+        $nextRepoUrl = '';
+        if (isset($oai->ListRecords->resumptionToken)
+            && $oai->ListRecords->resumptionToken != ''
         ) {
-            $JSON .= <<< EOT3
-    ,"contributor": {
-      "@type": "Organization",
-      "legalName": "$publisher"
-    }
-EOT3;
+            $nextRepoUrl = str_replace(
+                'metadataPrefix=oai_dc',
+                'resumptionToken=' . $oai->ListRecords->resumptionToken,
+                $repoURL
+            );
         }
-
-        if (isset($keywordString)
-            && $keywordString != ''
-        ) {
-            $JSON .= <<< EOT4
-    ,"keywords": [$keywordString]
-EOT4;
-        }
-        $JSON.= '
-}';
-        $outputJSON[] = $JSON;
-    }
-
-    //make the url for the next page if there is any
-    $nextRepoUrl = '';
-    if (isset($oai->ListRecords->resumptionToken)
-        && $oai->ListRecords->resumptionToken != ''
-    ) {
-        $nextRepoUrl = str_replace(
-    'metadataPrefix=oai_dc',
-            'resumptionToken=' . $oai->ListRecords->resumptionToken,
-            $repoURL
-        );
+    } catch (Exception $e) {
+        print 'there seems to be a problemo....' . $e->getMessage() . "\n";
+        exit(1);
     }
 }
 
