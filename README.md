bf4b555d750371dec6e5ba1c38f2839e6ad1bf42
a.lambert@unesco.org <r3d!zeb?raGi>
2021-06-08 15:33:10 +0200

    https://book.oceaninfohub.org/appendix/graphpub.html

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # OIHAquadocsParser
 get the information from the Aquadocs repository OAI interface and make it available for OIH in JSON-LD format
 
-For the time being this has been installed on the Wordpress server (mainly because the OIH site is installed there).
+This script is installed on the Wordpress server (mainly because the OIH site is installed there).
+The main reason for this is that we cannot change the HTML pages on the DSpace Server that is managed by Atmire.
+For this to work the ODIS-Arch has been adopted to accept graphs (https://book.oceaninfohub.org/appendix/graphpub.html)
 
-Can be found in /var/www/html/oih.aquadocs.org.
+Can be found in /var/www/html/oih.aquadocs.org on the wordpress.iode.vliz.be server.
 
-Result can be found/should be available,  via https://oih.aquadocs.org/aquadocs.json.
+The resulting graph can be found/should be available, via https://oih.aquadocs.org/aquadocs.json.
--- scripts/parser.php
+++ scripts/parser.php
@@ -138,33 +138,33 @@ while ($nextRepoUrl != '') {
             }
 
             $JSON = array(
-                "@context" => array(
-                    "@vocab" =>  "https://schema.org/"
+                '@context' => array(
+                    '@vocab' =>  'https://schema.org/'
                 ),
-                "@type" => "CreativeWork",
-                "@id" => "$identifier",
-                "name" => "$name",
-                "description" => "$descriptionString",
-                "url" =>  "$url",
-                "identifier" => array(
-                    "@id" => "https://hdl.handle.net/$idSuffix",
-                    "@type" => "PropertyValue",
-                    "propertyID" => "https://hdl.handle.net/",
-                    "value" => "$idSuffix",
-                    "url" => "https://hdl.handle.net/$idSuffix"
+                '@type' => 'CreativeWork',
+                '@id' => "$identifier",
+                'name' => "$name",
+                'description' => "$descriptionString",
+                'url' =>  "$url",
+                'identifier' => array(
+                    '@id' => "https://hdl.handle.net/$idSuffix",
+                    '@type' => 'PropertyValue',
+                    'propertyID' => 'https://hdl.handle.net/',
+                    'value' => "$idSuffix",
+                    'url' => "https://hdl.handle.net/$idSuffix"
                 )
             );
 
             /*
              * authors/creators
              * ODIS Arch expects
-             * "author": {
-             *      "@id": "https://www.sample-data-repository.org/person/51317",
-             *      "@type": "Person",
-             *      "name": "Dr Uta Passow",
-             *      "givenName": "Uta",
-             *      "familyName": "Passow",
-             *      "url": "https://www.sample-data-repository.org/person/51317"
+             * 'author': {
+             *      '@id': 'https://www.sample-data-repository.org/person/51317',
+             *      '@type': 'Person',
+             *      'name': 'Dr Uta Passow',
+             *      'givenName': 'Uta',
+             *      'familyName': 'Passow',
+             *      'url': 'https://www.sample-data-repository.org/person/51317'
              * },
              */
             $authors = array();
@@ -175,8 +175,8 @@ while ($nextRepoUrl != '') {
                     array_push(
                 $authors,
                          array(
-                            "@type" => "Person",
-                            "name" => "$creator"
+                            '@type' => 'Person',
+                            'name' => "$creator"
                         )
                     );
                 }
@@ -194,8 +194,8 @@ while ($nextRepoUrl != '') {
                 array_push(
                     $contributors,
                      array(
-                        "@type" => "Organization",
-                        "name" => "$publisher"
+                        '@type' => 'Organization',
+                        'name' => "$publisher"
                     )
                 );
             }
@@ -207,156 +207,9 @@ while ($nextRepoUrl != '') {
             if (isset($keywords)
                 && count($keywords)
             ) {
-                $JSON["keywords"] = $keywords;
+                $JSON['keywords'] = $keywords;
             }
             $outputJSON[] = $JSON;
-
-            /*
-            //print "\n*************************\n";
-            //print $record->header->identifier . ' ' . $record->header->datestamp . "\n";
-
-            //substract the handle id from the identifier
-            $identifier = $record->header->identifier;
-            $idSuffix = preg_replace(
-                '/.+?:(\d+\/\d+)/',
-                "$1",
-                $identifier
-            );
-
-            //to what (sub)sets belongs this document
-            foreach ($record->header->setSpec as $set) {
-                //print $set . "\n";
-            }
-
-            //read all the metadata and put the values in vars ready for output
-
-            //we often use the same regex
-            $pattern = array(
-                '"',
-                "\n",
-                "\r",
-                "\t"
-            );
-            $replace = array(
-                '\"',
-                ' ',
-                ' ',
-                ' '
-            );
-            $metaData = $record->metadata->oai_dc_dc;
-            $name = trim($metaData->dc_title);
-            $name = str_replace(
-                $pattern,
-                $replace,
-                $name
-            );
-
-            //get all the descriptions
-            //put them in one long description
-            $descriptions = array();
-            $descriptionString = '';
-            foreach ($metaData->dc_description as $description) {
-                $description = str_replace(
-                    $pattern,
-                    $replace,
-                    trim($description)
-                );
-                $descriptions[] = ' - ' . $description . ' - ';
-            }
-            $descriptionString = implode(', ', $descriptions);
-
-            //this is the link to the document
-            $url = 'https://www.oceandocs.org/handle/' . $idSuffix;
-
-            //get all the contributors
-            $contributors = array();
-            foreach ($metaData->dc_creator as $contributor) {
-                $contributor = str_replace(
-                    $pattern,
-                    $replace,
-                    trim($contributor)
-                );
-                $contributors[] = $contributor;
-            }
-
-            //get all the keywords
-            $keywords = array();
-            $keywordString = '';
-            foreach ($metaData->dc_subject as $keyword) {
-                $keyword = str_replace(
-                    $pattern,
-                    $replace,
-                    trim($keyword)
-                );
-                $keywords[] = '"' . $keyword . '"';
-            }
-            $keyword = implode(', ', $keywords);
-
-            //special case for contributor
-            $publisher = '';
-            if (isset($metaData->dc_publisher)) {
-                $publisher = $metaData->dc_publisher;
-                $publisher = str_replace(
-                    $pattern,
-                    $replace,
-                    trim($publisher)
-                );
-            }
-
-            $JSON = <<<EOT
-    {
-        "@context": {
-            "@vocab": "https://schema.org/"
-        },
-        "@type": "CreativeWork",
-        "@id": "$identifier",
-        "name": "$name",
-        "description": "$descriptionString",
-        "url":  "$url",
-        "identifier":
-          {
-            "@id": "https://handle.net/$idSuffix",
-            "@type": "PropertyValue",
-            "propertyID": "https://handle.net/",
-            "value": "$idSuffix",
-            "url": "https://handle.net/$idSuffix"
-          }
-    EOT;
-            if (isset($contributors)
-                && count($contributors)
-            ) {
-                foreach ($contributors as $contributor) {
-                    $JSON .= <<< EOT2
-        ,"contributor": {
-          "@type": "Person",
-          "name": "$contributor"
-        }
-    EOT2;
-                }
-            }
-
-            if (isset($publisher)
-                && $publisher != ''
-            ) {
-                $JSON .= <<< EOT3
-        ,"contributor": {
-          "@type": "Organization",
-          "legalName": "$publisher"
-        }
-    EOT3;
-            }
-
-            if (isset($keywordString)
-                && $keywordString != ''
-            ) {
-                $JSON .= <<< EOT4
-        ,"keywords": [$keywordString]
-    EOT4;
-            }
-            $JSON .= '
-    }';
-            $outputJSON[] = $JSON;
-            */
         }
 
         //make the url for the next page if there is any
@@ -376,8 +229,25 @@ while ($nextRepoUrl != '') {
     }
 }
 
-/*
-$output = '[' . implode(', ', $outputJSON) . ']';
-file_put_contents('../aquadocs.json', $output);
-*/
-file_put_contents(dirname(__FILE__) . '/../aquadocs.json', json_encode($outputJSON, JSON_PRETTY_PRINT));
+$outputGraph = array(
+    '@context' => array(
+        '@vocab' =>  'https://schema.org/'
+    ),
+    '@type' => array(
+        'ItemList',
+        'CreativeWork'
+    ),
+    'name' => 'Resource collection for AquaDocs.org',
+    'author' => 'Arno Lambert <a.lambert@unesco.org>',
+    'itemListOrder' => 'https://schema.org/ItemListUnordered',
+    'numberOfItems' =>  count($outputJSON),
+    'itemListElement' => $outputJSON
+);
+//finally save the file to be accessed
+file_put_contents(
+        dirname(__FILE__) . '/../aquadocs.json',
+        json_encode(
+                $outputGraph,
+                JSON_PRETTY_PRINT
+        )
+);
